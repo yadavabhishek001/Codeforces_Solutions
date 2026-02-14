9bc43e3cf76b35a0ac69b15bfcc00ed8dfae38b0
leetcode-solutions <abhasnp55@gmail.com>
2026-02-14 20:34:40 +0530

    updating

--- Problem_Newbie/4A_watermelon.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
- 
-public class 4A_watermelon{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.print(n%2 == 0 && n>2 ? "Yes" : "No");
-    }
-}
