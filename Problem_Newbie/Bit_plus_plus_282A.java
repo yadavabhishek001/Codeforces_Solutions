460a06b8374f3dd88dffff1d770679a4df06099d
leetcode-solutions <abhasnp55@gmail.com>
2026-02-17 20:17:41 +0530

    updating

--- Problem_Newbie/Bit_plus_plus_282A.java
+++ Problem_Newbie/Bit_plus_plus_282A.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Bit++_228A {
+public class Bit_plus_plus_282A {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
