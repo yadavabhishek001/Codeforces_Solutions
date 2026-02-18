1ee76e782d4e478d6a4a7f04523421eab125209c
leetcode-solutions <abhasnp55@gmail.com>
2026-02-18 21:29:56 +0530

    Adding solution of 546A

---
+++ Problem_Newbie/Soldiers_and_bananas_546A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Soldiers_and_bananas_546A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int cost = sc.nextInt();
+        int dollers = sc.nextInt();
+        int banana = sc.nextInt();
+        int sum = 0;
+        for(int i = 1; i<=banana ;i++) {
+            int a = cost*i;
+            sum  += a;
+        }
+        if(sum <= dollers){
+         System.out.println(0);
+        }
+        else{
+         System.out.println(sum - dollers);
+        }
+    }
+}
