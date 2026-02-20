501e33e5bb4fb90e70b750067737375e2e95fd1f
leetcode-solutions <abhasnp55@gmail.com>
2026-02-20 21:09:44 +0530

    Adding solution of Transelation_41A

---
+++ Problem_Newbie/Transelation_41A.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Transelation_41A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.next();
+        String str1 = sc.next();
+        int n = str1.length();
+        int n1 = str.length();
+        
+        if(n1 != n){
+            System.out.println("NO");
+            return;
+        }
+        
+        int j =0;
+        for(int i = n-1; i>=0 ; i--){
+            if( str1.charAt(i) != str.charAt(j) ){
+                System.out.println("NO");
+                return;
+            }
+            j++;
+        }
+        System.out.println("YES");
+        sc.close();
+    }
+}
