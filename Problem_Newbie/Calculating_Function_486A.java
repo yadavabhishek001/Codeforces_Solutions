bbda78e73643c726c410b07d409285590c7b2305
leetcode-solutions <abhasnp55@gmail.com>
2026-03-10 22:59:59 +0530

    Added solution of Q486A

---
+++ Problem_Newbie/Calculating_Function_486A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Calculating_Function_486A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        if(n % 2 == 0){
+         System.out.print(n/2);
+        }
+        else{
+            long a = (-1)*(n/2) - 1 ;
+            System.out.print(a);
+        }
+    }
+}
