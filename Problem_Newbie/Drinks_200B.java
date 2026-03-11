72d613a7b566d6f201d42ea90ebe8a22e31b9ea1
leetcode-solutions <abhasnp55@gmail.com>
2026-03-11 20:19:57 +0530

    Adding Solution of Q200B

---
+++ Problem_Newbie/Drinks_200B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Drinks_200B {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0 ; i<n ; i++){
+            int a = sc.nextInt();
+            sum += a;
+        }
+        double x = (double) sum/n;
+        System.out.printf( "%.12f%n" , x);
+    }
+}
