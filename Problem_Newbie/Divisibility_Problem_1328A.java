dd792e563125597f8d7fb302f95f28c55f93db47
leetcode-solutions <abhasnp55@gmail.com>
2026-03-11 20:24:58 +0530

    Added solution fo Q1328A

---
+++ Problem_Newbie/Divisibility_Problem_1328A.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Divisibility_Problem_1328A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0 ; i<n ; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a % b == 0){
+             System.out.println("0");
+            }
+            else{
+                int x = a % b;
+                x = b - x;
+             System.out.println(x);
+            }
+        }
+        
+    }
+}
