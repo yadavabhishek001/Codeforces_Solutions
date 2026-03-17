d682574f6ca7cd991d644f46a4dbf52ccc018db3
leetcode-solutions <abhasnp55@gmail.com>
2026-03-17 22:12:19 +0530

    Added solution of Q1335A

---
+++ Problem_Newbie/Candies_and_Two_Sisters_1335A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Candies_and_Two_Sisters_1335A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0 ; i<n ; i++){
+            int a = sc.nextInt();
+            if(a <= 2){
+                System.out.println("0");
+            }
+            else{
+                if(a % 2 == 0){
+                    int b = (a/2) - 1;
+                    System.out.println(b);
+                }
+                else{
+                    System.out.println(a/2);
+                }
+            }
+        }
+        sc.close();
+        
+    }
+}
