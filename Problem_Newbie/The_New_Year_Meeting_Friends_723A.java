1c75ea914f3c20c91963148472d14f920d39130a
leetcode-solutions <abhasnp55@gmail.com>
2026-03-17 22:13:23 +0530

    Added solution of Q723A

---
+++ Problem_Newbie/The_New_Year_Meeting_Friends_723A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class The_New_Year_Meeting_Friends_723A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        
+        int res = Math.max(n1 , Math.max(n2,n3) ) - Math.min(n1 , Math.min(n2,n3) );
+        System.out.println(res);
+        
+        sc.close();
+    }
+}
