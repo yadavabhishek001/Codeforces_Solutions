bba22c515f20fda903b46ede0dace75fa86bb1ed
Abhishek Yadav <abhasnp55@gmail.com>
2026-03-18 23:15:37 +0530

    Added solution of Restoring_Three_Numbers_1154A

---
+++ Problem_Newbie/Restoring_Three_Numbers_1154A.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Restoring_Three_Numbers_1154A{
+    public static void main (String args[]){
+     Scanner sc = new Scanner(System.in);
+     int arr[] = new int[4];
+     int max = 0;
+     int a = -1;
+     for( int i = 0; i < 4 ; i++){
+         arr[i] = sc.nextInt();
+         if(arr[i] > max){
+             max = arr[i];
+             a = i;
+         }
+     }
+     for(int i=0 ; i<4 ; i++){
+         if(i == a){
+             continue;
+         }
+         System.out.print((max - arr[i]) + " ");
+     }
+    }
+}
