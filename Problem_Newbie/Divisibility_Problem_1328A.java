f178595a099625b1b079046c7c58c354c1c6acb2
leetcode-solutions <abhasnp55@gmail.com>
2026-03-18 23:21:52 +0530

    Updating

--- Problem_Newbie/Divisibility_Problem_1328A.java
+++ Problem_Newbie/Divisibility_Problem_1328A.java
@@ -4,7 +4,6 @@ class Divisibility_Problem_1328A {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum = 0;
         for(int i = 0 ; i<n ; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
@@ -17,6 +16,7 @@ class Divisibility_Problem_1328A {
              System.out.println(x);
             }
         }
+        sc.close();
         
     }
 }
