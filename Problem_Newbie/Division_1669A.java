4821db171ff1732cfc2eb519870a6f6d41fcad4d
leetcode-solutions <abhasnp55@gmail.com>
2026-03-18 23:06:58 +0530

    Added solution of Q1669A

---
+++ Problem_Newbie/Division_1669A.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Division_1669A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i<t ;i++){
+            int a = sc.nextInt();
+            
+            if(a <= 1399){
+                System.out.println("Division 4");
+            }
+            else if(a <= 1599  && a >= 1400){
+                System.out.println("Division 3");
+            }
+            else if(a <= 1899 && a >= 1600){
+                System.out.println("Division 2");
+            }
+            else
+            {
+                System.out.println("Division 1");
+            }
+        }
+        sc.close();
+    }
+}
