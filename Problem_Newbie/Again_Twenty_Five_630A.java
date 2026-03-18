029296b583b987f2a05862c9b84e5e86cd770bd8
leetcode-solutions <abhasnp55@gmail.com>
2026-03-18 23:06:35 +0530

    Added solution of 630A

---
+++ Problem_Newbie/Again_Twenty_Five_630A.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Again_Twenty_Five_630A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        sc.nextLong();
+        System.out.print(25);
+
+        sc.close();
+    }
+}
