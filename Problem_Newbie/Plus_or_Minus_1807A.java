bd7b4ad90d76c9dfcb698ff3f5a9d50c2f1e450a
leetcode-solutions <abhasnp55@gmail.com>
2026-03-23 21:37:39 +0530

    Added solution of 1807A

---
+++ Problem_Newbie/Plus_or_Minus_1807A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Plus_or_Minus_1807A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i<t ;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            
+            if(a + b == c){
+                System.out.println("+");
+            }
+            else{
+                System.out.println("-");
+            }
+        }
+        sc.close();
+    }
+}
