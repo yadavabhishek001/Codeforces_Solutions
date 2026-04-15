31090a7ac7f66b83ff9cd090c014b0cb73cec1cd
leetcode-solutions <abhasnp55@gmail.com>
2026-04-15 06:10:21 +0530

    Added solution of 1900A

---
+++ Problem_Newbie/A_Cover_in_Water.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class A_Cover_in_Water{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+            String s = sc.next();
+            char prev = s.charAt(0);
+            int count = 0;
+            int sum = 0;
+            int maxCount = 0;
+            for(int i = 0 ; i<n ; i++){
+                char ch = s.charAt(i);
+                if( (i>0 && prev == '.' && ch == '.') || ch == '.' ){
+                    count += 1;
+                    sum += 1;
+                }
+                else{
+                    count = 0;
+                }
+                maxCount = Math.max(maxCount, count);
+                prev = ch;
+            }
+            if(maxCount>2){
+                System.out.println("2");
+            }
+            else{
+                System.out.println(sum);
+            }
+        }
+        sc.close();
+    }
+}
