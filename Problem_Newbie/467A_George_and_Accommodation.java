9351ae0f656aaa2d5a1fc0744a6aeba87922baf4
Abhishek Yadav <abhasnp55@gmail.com>
2026-04-21 19:58:04 +0530

    Added solution of 567A

--- Problem_Newbie/467A_George_and_Accommodation.java
+++ Problem_Newbie/467A_George_and_Accommodation.java
@@ -4,6 +4,7 @@ class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int sum = 0;
+        
         for(int i = 0; i<n ;i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
