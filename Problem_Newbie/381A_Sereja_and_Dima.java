6733f386307166d5664b61c519feea7b468f5f31
Abhishek Yadav <abhasnp55@gmail.com>
2026-04-24 20:11:45 +0530

    Add Sereja and Dima game implementation

---
+++ Problem_Newbie/381A_Sereja_and_Dima.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int l = 0, r = n - 1;
+        int sereja = 0, dima = 0;
+        boolean turn = true; // true = Sereja, false = Dima
+
+        while (l <= r) {
+            int picked;
+
+            if (a[l] > a[r]) {
+                picked = a[l];
+                l++;
+            } else {
+                picked = a[r];
+                r--;
+            }
+
+            if (turn) {
+                sereja += picked;
+            } else {
+                dima += picked;
+            }
+
+            turn = !turn; // switch turn
+        }
+
+        System.out.println(sereja + " " + dima);
+    }
+}
