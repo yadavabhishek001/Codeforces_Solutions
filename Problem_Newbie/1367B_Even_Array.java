aaf302679be629e2602e348e2425a9ef1480d93c
Abhishek Yadav <abhasnp55@gmail.com>
2026-04-29 19:30:10 +0530

    Added solution of 1676B
    Fix formatting issues in Even_Array.java.

--- Problem_Newbie/1367B_Even_Array.java
+++ Problem_Newbie/1367B_Even_Array.java
@@ -11,8 +11,7 @@ public class Main {
 
             for (int i = 0; i < n; i++) {
                 a[i] = sc.nextInt();
-            }
-            
+            } 
             int evenIndexOddValue = 0;
             int oddIndexEvenValue = 0;
 
