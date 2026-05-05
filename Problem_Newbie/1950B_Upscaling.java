aa91a536f0ce5214b3761c2e1d76cff0b302f333
Abhishek Yadav <abhasnp55@gmail.com>
2026-05-05 20:05:05 +0530

    Added solution of 1950B

---
+++ Problem_Newbie/1950B_Upscaling.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int size = 2 * n;
+
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if ((i / 2 + j / 2) % 2 == 0) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print(".");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
