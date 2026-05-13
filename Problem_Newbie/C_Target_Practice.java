3fda9b645012b775bbbfe2240b3a7437b3dc6203
Abhishek Yadav <abhasnp55@gmail.com>
2026-05-13 20:55:57 +0530

    Added solution of C_Target_Practice{

--- Problem_Newbie/C_Target_Practice.java
+++ Problem_Newbie/C_Target_Practice.java
@@ -11,6 +11,7 @@ public class C_Target_Practice{
             char ch[][] = new char[10][10];
             int sum = 0;
             for(int i = 0; i<10 ; i++){
+                
                 String row = sc.next();
                 for(int j = 0; j<10 ; j++){
                     
