5662f5bae7d1f5490a72021513982e5a14599d90
leetcode-solutions <abhasnp55@gmail.com>
2026-05-14 22:35:09 +0530

    Added solution of M_Minimum_LCM

---
+++ Problem_Newbie/M_Minimum_LCM.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class M_Minimum_LCM {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long t = scanner.nextLong();
+		while (t-- > 0) {
+			long n = scanner.nextLong();
+
+			long a = 1;
+			long b = n - 1;
+
+			for (int i = 2; i*i <= n ; i++ ) {
+				if(n % i == 0){
+					a = n / i;
+					b = n - a;
+					break;
+				}
+			}
+			System.out.println(a + " " + b);
+		}
+		scanner.close();
+	}
+
+	// Time Complexity (TC): O(sqrt(10^9)) = O(10^4)
+	// Space Complexity (SC): O(1)
+}
