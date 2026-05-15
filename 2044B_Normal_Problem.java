985d6ed4b8284a8b4b8bdf2ff0a31ed83be6173b
Abhishek Yadav <abhasnp55@gmail.com>
2026-05-15 22:25:35 +0530

    Added solutio of 2044B

---
+++ 2044B_Normal_Problem.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String a = sc.next();
+            StringBuilder b = new StringBuilder();
+
+            for (int i = a.length() - 1; i >= 0; i--) {
+                char ch = a.charAt(i);
+
+                if (ch == 'p') {
+                    b.append('q');
+                } else if (ch == 'q') {
+                    b.append('p');
+                } else {
+                    b.append('w');
+                }
+            }
+
+            System.out.println(b);
+        }
+    }
+}
