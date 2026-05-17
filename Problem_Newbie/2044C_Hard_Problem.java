d31e51b6105386b3d3f44477b9c7424a02bcb451
Abhishek Yadav <abhasnp55@gmail.com>
2026-05-17 19:26:17 +0530

    Added solution of 2044C

---
+++ Problem_Newbie/2044C_Hard_Problem.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long m = sc.nextLong();
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+
+            long row1 = Math.min(a, m);
+            long row2 = Math.min(b, m);
+
+            long remaining = 2 * m - row1 - row2;
+
+            long ans = row1 + row2 + Math.min(c, remaining);
+
+            System.out.println(ans);
+        }
+    }
+}
