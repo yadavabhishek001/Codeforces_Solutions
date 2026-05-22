f52e300ffe31401b8b366b36d840dae1a6037684
Abhishek Yadav <abhasnp55@gmail.com>
2026-05-22 17:24:30 +0530

    Added solution of 1806A

---
+++ Problem_Newbie/1806A_Walking_Master.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class A_Walking_Master{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+            long d = sc.nextLong();
+
+            long yDiff = d - b;
+            if(yDiff < 0){
+                System.out.println("-1");
+                continue;
+            }
+            long count = 0;
+
+            a = a + yDiff;
+            b = b + yDiff;
+            count = count + yDiff;
+            if(a<c){
+                System.out.println("-1");
+                continue;
+            }
+            count = count + (a - c);
+
+            System.out.println(count);
+        }
+    }
+}
