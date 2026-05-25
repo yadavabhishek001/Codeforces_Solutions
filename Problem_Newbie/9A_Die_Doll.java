85dc56b2387843dab584d63a295cbe15d4b200c0
Abhishek Yadav <abhasnp55@gmail.com>
2026-05-25 22:32:19 +0530

    Added solution of 9A

---
+++ Problem_Newbie/9A_Die_Doll.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int count = 0;
+        if(a>b){
+            for(int i = a ; i<=6 ; i++){
+                count++;
+            }
+        }
+        else{
+            for(int i = b; i<=6 ; i++){
+                count++;
+            }
+        }
+        int c = 6;
+        for(int i = 2; i<=6 ;i++){
+            if(count % i == 0 && 6 % i == 0){
+                count = count / i;
+                c = c/i;
+            }
+        }
+        System.out.println(count +"/" + c);
+        sc.close();
+    }
+}
