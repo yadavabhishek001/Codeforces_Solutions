ef65f4e86a960ce2e48e53ede7903c41c4406931
Abhishek Yadav <abhasnp55@gmail.com>
2026-05-25 22:27:01 +0530

    Added solution of 1703B

---
+++ Problem_Newbie/1703B_ICPC_Ballons.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+
+            boolean[] seen = new boolean[26];
+            int balloons = 0;
+
+            for (char c : s.toCharArray()) {
+                int idx = c - 'A';
+
+                if (!seen[idx]) {
+                    balloons += 2;
+                    seen[idx] = true;
+                } else {
+                    balloons += 1;
+                }
+            }
+
+            System.out.println(balloons);
+        }
+    }
+}
