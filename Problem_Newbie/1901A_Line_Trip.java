ca097ac3900d933ecbc38300b1629a3a1f43f21e
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-03 17:48:22 +0530

    Added solution of 1901A

---
+++ Problem_Newbie/1901A_Line_Trip.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class A_Line_Trip {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-->0){
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int maxDist = 0;
+            int dist = 0;
+            int prev = 0;
+            for(int i = 0; i<n ; i++){
+                int a = sc.nextInt();
+                
+                dist = a-prev;
+                if(i == n-1){
+                    dist = Math.max(dist, 2*(x-a));
+                }
+                maxDist = Math.max(maxDist,dist);
+                prev = a;
+            }
+            System.out.println(maxDist);
+        }
+    }
+}
