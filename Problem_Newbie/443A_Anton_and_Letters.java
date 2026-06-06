ff19614f10da8859ff811d9a1fac16d9dd6e617a
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-06 21:24:12 +0530

    Rename Anton_and_Letters_443A.java to 443A_Anton_and_Letters.java

--- Problem_Newbie/443A_Anton_and_Letters.java
+++ Problem_Newbie/443A_Anton_and_Letters.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.HashSet;
 
-public class Anton_and_Letters_443A {
+public class 443A_Anton_and_Letters {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
