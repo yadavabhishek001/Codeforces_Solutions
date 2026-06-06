8cf12ca49e623c735733cbe81256f186b2b96cc2
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-06 21:23:38 +0530

    Rename Petya_and_String_112A.java to 112A_Petya_and_String.java

--- Problem_Newbie/112A_Petya_and_String.java
+++ Problem_Newbie/112A_Petya_and_String.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Petya_and_String_112A {
+public class Petya_and_String {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.next();
