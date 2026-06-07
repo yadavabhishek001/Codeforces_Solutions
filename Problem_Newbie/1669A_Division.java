7b22f160465d4c9061a7561043be470ef19ad99a
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-07 22:37:16 +0530

    Rename Division_1669A.java to 1669A_Division.java

--- Problem_Newbie/1669A_Division.java
+++ Problem_Newbie/1669A_Division.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Division_1669A {
+public 1669A_class Division {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
