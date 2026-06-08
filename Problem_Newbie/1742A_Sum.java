74edf52711b8ce3144352d6ad1279f2637f2e8c8
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-08 18:18:53 +0530

    Add new file 1742A_Sum.java

--- Problem_Newbie/1742A_Sum.java
+++ Problem_Newbie/1742A_Sum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Sum_1742A {
+class 1742A_Sum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
