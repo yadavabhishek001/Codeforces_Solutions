f8c0a35fd0c6fb8349e69d202457f809b4ca3d53
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-09 21:31:37 +0530

    Update and rename Bear_and_Big_Brother_791A.java to 791A_Bear_and_Big_Brother.java

--- Problem_Newbie/791A_Bear_and_Big_Brother.java
+++ Problem_Newbie/791A_Bear_and_Big_Brother.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Bear_and_Big_Brother_791A{  
+public class 791A_Bear_and_Big_Brother{  
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
