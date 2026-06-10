b89ac8cf4c40e3fc1a5d19d60a49ae5660dda2a9
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-10 22:59:46 +0530

    Rename Elephant_617A.java to 617A_Elephant.java

--- Problem_Newbie/617A_Elephant.java
+++ Problem_Newbie/617A_Elephant.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Elephant_617A {
+public class 617A_Elephant {
     public static void main(String args[]){
      Scanner sc = new Scanner (System.in);
      int n = sc.nextInt();
