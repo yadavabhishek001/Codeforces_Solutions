fdb8fae578fad3d56d2c437bbe771b6a5186ced6
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-10 22:57:47 +0530

    Rename class Drinks_200B to 200B_Drinks

--- Problem_Newbie/200B_Drinks.java
+++ Problem_Newbie/200B_Drinks.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Drinks_200B {
+public class 200B_Drinks {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
