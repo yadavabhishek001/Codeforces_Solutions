3263bf3aff95c2055a38365588c73c351a87faef
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-10 23:00:53 +0530

    Rename file to 344A_Magnets.java

--- Problem_Newbie/344A_Magnets.java
+++ Problem_Newbie/344A_Magnets.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Magnets_344A {
+class 344A_Magnets {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
