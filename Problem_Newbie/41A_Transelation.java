6f37414bb9dcd57d6259a2db77c24234a0957131
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-10 21:26:28 +0530

    Rename class from Transelation_41A to 41A_Transelation

--- Problem_Newbie/41A_Transelation.java
+++ Problem_Newbie/41A_Transelation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Transelation_41A {
+public class 41A_Transelation{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
