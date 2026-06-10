b80b1ee85c98c201baf5437a0172ed15309fb22d
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-10 21:24:45 +0530

    Rename class from Word_59A to 59A_Word

--- Problem_Newbie/59A_Word.java
+++ Problem_Newbie/59A_Word.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Word_59A {
+public class 59A_Word {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
