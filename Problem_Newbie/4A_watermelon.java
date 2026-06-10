862e8e2914b82f28c7edfa67f7907a504deb0f28
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-10 21:23:46 +0530

    Rename watermelon_4A.java to 4A_watermelon.java

--- Problem_Newbie/4A_watermelon.java
+++ Problem_Newbie/4A_watermelon.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class watermelon_4A{
+public class 4A_watermelon{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
