075d95ccdaacd09955eacb5d18bbc724dd911458
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-10 22:58:48 +0530

    Rename Marathan_1602A.java to 1602A_Marathan.java

--- Problem_Newbie/1602A_Marathan.java
+++ Problem_Newbie/1602A_Marathan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Marathan_1602A {
+class 1602A_Marathan {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
