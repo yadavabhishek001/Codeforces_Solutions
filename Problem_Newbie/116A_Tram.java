0324ee9902daf79ec37f7065ac0a54ea108a0dce
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-10 23:00:17 +0530

    Update and rename Tram_116A.java to 116A_Tram.java

--- Problem_Newbie/116A_Tram.java
+++ Problem_Newbie/116A_Tram.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Tram_116A {
+public class 116A_Tram {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
