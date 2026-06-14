f2c3a4f34d80c49f6780b670e6284616126a2ae0
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-14 20:37:09 +0530

    Rename class to 128A_Next_Round in Java file

--- Problem_Newbie/128A_Next_Round.java
+++ Problem_Newbie/128A_Next_Round.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Next_Round_128A {
+public class 128A_Next_Round{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // int t = sc.nextInt();
