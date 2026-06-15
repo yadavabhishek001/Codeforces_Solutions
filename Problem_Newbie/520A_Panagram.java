c5cc473ed83c47e447bcda13c87179cb7e79f503
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-15 14:18:35 +0530

    Rename Panagram_520A.java to 520A_Panagram.java

--- Problem_Newbie/520A_Panagram.java
+++ Problem_Newbie/520A_Panagram.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Scanner;
 
-class Panagram_520A {
+class 520A_Panagram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
