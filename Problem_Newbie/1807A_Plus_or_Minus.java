10f0ff34ef1cb436aff7a6704358c3f536e2d834
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-15 14:19:54 +0530

    Rename Plus_or_Minus_1807A.java to 1807A_Plus_or_Minus.java

--- Problem_Newbie/1807A_Plus_or_Minus.java
+++ Problem_Newbie/1807A_Plus_or_Minus.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Plus_or_Minus_1807A{
+class 1807A_Plus_or_Minus{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
