b986dc08071a8d62af56e54e4acab0d96b1704fc
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-16 11:33:50 +0530

    Rename Soldiers_and_bananas_546A.java to 546A_Soldiers_and_bananas.java

--- Problem_Newbie/546A_Soldiers_and_bananas.java
+++ Problem_Newbie/546A_Soldiers_and_bananas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Soldiers_and_bananas_546A {
+public class 546A_Soldiers_and_bananas {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int cost = sc.nextInt();
