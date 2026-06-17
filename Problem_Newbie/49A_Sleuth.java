b26c852d00ee92e7eb1aaeb486fa5c65a97f5a1f
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-17 17:01:34 +0530

    Rename Sleuth_49A.java to 49A_Sleuth.java

--- Problem_Newbie/49A_Sleuth.java
+++ Problem_Newbie/49A_Sleuth.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Sleuth_49A {
+public class 49A_Sleuth {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String sentence = sc.nextLine();
