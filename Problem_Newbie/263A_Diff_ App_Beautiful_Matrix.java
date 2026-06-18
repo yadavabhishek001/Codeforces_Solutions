a6981aa1a74fd4f815aa4419906c07b05feaaef4
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-18 22:19:25 +0530

    Add new file for Beautiful Matrix problem solution

--- Problem_Newbie/263A_Diff_ App_Beautiful_Matrix.java
+++ Problem_Newbie/263A_Diff_ App_Beautiful_Matrix.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class A_Beautiful_Matrix {
+public class 263A_Beautiful_Matrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] matrix = new int[5][5];
