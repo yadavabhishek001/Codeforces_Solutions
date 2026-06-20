6f3d33d8d730bd90738ad57aaf2d7071430ee609
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-20 13:27:31 +0530

    Refactor button comparison logic in 1858A_Buttons
    Refactor logic to compare button values and handle odd/even cases for the third button.

--- Problem_Newbie/1858A_Buttons_diff_App.java
+++ Problem_Newbie/1858A_Buttons_diff_App.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
 
-class A_Buttons{
+public class Buttons {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int t = sc.nextInt();
 
-        while(t-- > 0){
+        while (t-- > 0) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int c = sc.nextInt();
 
-            int bSum = b + c/2;
-            int aSum = a + (c - c/2);
-
-            if(aSum > bSum){
+            if (a > b) {
                 System.out.println("First");
-            }
-            else{
+            } 
+            else if (b > a) {
                 System.out.println("Second");
+            } 
+            else {
+                if (c % 2 == 1) {
+                    System.out.println("First");
+                } else {
+                    System.out.println("Second");
+                }
             }
-            
         }
+
         sc.close();
     }
 }
