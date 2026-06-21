b71f3e320019b03b7c230ffedaa6ea4bd8dda3a2
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-21 18:17:46 +0530

    Rename class to 1873C_Target_Practice

--- Problem_Newbie/1873C_Target_Practice.java
+++ Problem_Newbie/1873C_Target_Practice.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class C_Target_Practice{
+public class 1873C_Target_Practice{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
