27d3cda3fb186e1fe71dc8f0639ac97891f9aa22
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:58:01 +0530

    Add osu!mania [B] from Codeforces

---
+++ 2009/B - osu!mania/solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String[] grid = new String[n];
+ 
+            for (int i = 0; i < n; i++) {
+                grid[i] = sc.next();
+            }
+ 
+            // Process from bottom to top
+            for (int i = n - 1; i >= 0; i--) {
+                for (int j = 0; j < 4; j++) {
+                    if (grid[i].charAt(j) == '#') {
+                        System.out.print((j + 1) + " ");
+                        break;
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
