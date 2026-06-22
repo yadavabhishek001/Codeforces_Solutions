edb2b267de94fb9056bac47e757880feb1462159
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:56:57 +0530

    Add Remove Smallest [A] from Codeforces

---
+++ 1399/A - Remove Smallest/solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+ 
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+ 
+            Arrays.sort(a);
+ 
+            boolean possible = true;
+ 
+            for (int i = 1; i < n; i++) {
+                if (a[i] - a[i - 1] > 1) {
+                    possible = false;
+                    break;
+                }
+            }
+ 
+            if (possible) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+ 
+        sc.close();
+    }
+}
