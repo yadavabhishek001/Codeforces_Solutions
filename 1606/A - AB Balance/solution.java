83288645576618e6e7b74dc0bd26e7811c0218d0
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:49:19 +0530

    Add AB Balance [A] from Codeforces

---
+++ 1606/A - AB Balance/solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class A_AB_Balance{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            String s = sc.next();
+ 
+            int n = s.length();
+            
+            if( s.charAt(0) != s.charAt(n-1)){
+                if(s.charAt(0) == 'a'){
+                    s = 'b'  + s.substring(1);
+                }
+                else{
+                    s = 'a' +  s.substring(1);
+                }
+            }
+            System.out.println(s);
+        }
+        sc.close();
+    }
+}
