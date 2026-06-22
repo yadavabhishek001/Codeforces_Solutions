ef609e7ba242277910ea555df635ad7976783562
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:42:51 +0530

    Add Halloumi Boxes [A] from Codeforces

---
+++ 1903/A - Halloumi Boxes/solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class A_Halloumi_Boxes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            boolean flag = true;
+            int prev = -1;
+            for(int i = 0; i<n ; i++){
+                int a = sc.nextInt();
+                if(i>=1 && prev > a ){
+                    flag = false;
+                }
+                prev = a;
+            }
+            if( flag && k == 1 ){
+                System.out.println("YES");
+            }
+            else if(k > 1){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
