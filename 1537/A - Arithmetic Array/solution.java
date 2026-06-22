b5a5f74001ac4abbcb8da2ba124b8bfb292fe1e2
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 19:03:29 +0530

    Add Arithmetic Array [A] from Codeforces

---
+++ 1537/A - Arithmetic Array/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+ 
+            int sum = 0;
+ 
+            for (int i = 0; i < n; i++) {
+                sum += sc.nextInt();
+            }
+ 
+            if (sum == n) {
+                System.out.println(0);
+            } 
+            else if (sum < n) {
+                System.out.println(1);
+            } 
+            else {
+                System.out.println(sum - n);
+            }
+        }
+    }
+}
