f1ba58d5f51f96fb42d3620aa826a4b047fff28c
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:58:55 +0530

    Add X Axis [A] from Codeforces

---
+++ 1986/A - X Axis/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int x3 = sc.nextInt();
+ 
+            int max = Math.max(x1, Math.max(x2, x3));
+            int min = Math.min(x1, Math.min(x2, x3));
+ 
+            int result = max - min;
+            System.out.println(result);
+        }
+    }
+}
