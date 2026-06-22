4f2b2f3282cfac10736035bdaa90353ee8a68668
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:59:04 +0530

    Add Problem Generator [A] from Codeforces

---
+++ 1980/A - Problem Generator/solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            String s = sc.next();
+ 
+            int[] freq = new int[7]; // A-G
+ 
+            // Count frequencies
+            for (char c : s.toCharArray()) {
+                freq[c - 'A']++;
+            }
+ 
+            int needed = 0;
+ 
+            // Check each difficulty A to G
+            for (int i = 0; i < 7; i++) {
+                if (freq[i] < m) {
+                    needed += (m - freq[i]);
+                }
+            }
+ 
+            System.out.println(needed);
+        }
+    }
+}
