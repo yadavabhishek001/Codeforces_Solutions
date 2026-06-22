8da02bb2ca98a88e261ddc0f259ea1fc800642f0
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:41:06 +0530

    Add Short Substrings [A] from Codeforces

---
+++ 1367/A - Short Substrings/solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<t ; i++){
+            String s = sc.next();
+            
+            int n = s.length();
+            sb.append( s.charAt(0) );
+            for(int j = 1; j<n-1 ; j++){
+                if(j % 2 != 0){
+                    sb.append( s.charAt(j) );
+                }
+            }
+            sb.append(s.charAt(n-1) );
+            
+            System.out.println(sb.toString());
+            sb.setLength(0);
+        }
+    }
+}
