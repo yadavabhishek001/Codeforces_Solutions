6df275695326b2dd6fa589baf022380c3aa314a2
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:33:10 +0530

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for(int i = 0; i<n ; i++){
+            String s = sc.next();
+            if(s.equals("X++") || s.equals("++X")){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        System.out.println(count);
+    }
+}
