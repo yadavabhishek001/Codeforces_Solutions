d07d32f6118fbfe257d39e66e6e499c8fe504ca4
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:35:24 +0530

    Add Police Recruits [A] from Codeforces

---
+++ 427/A - Police Recruits/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int count = 0;
+        int sum = 0;
+        for(int i = 0; i<t ; i++){
+            int a = sc.nextInt();
+            if(sum <= 0 && a == -1){
+                count++;
+            }
+            else if(sum > 0 && a == -1){
+                sum += a;
+            }
+            else if(sum >= 0 && a > 0){
+                sum += a;
+            }
+        }
+        System.out.print(count);
+    }
+}
