e4219a7309adbffe4c56ba3c4f2dd3841b1c5b20
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:54:20 +0530

    Add Maximum Multiple Sum [B] from Codeforces

---
+++ 1985/B - Maximum Multiple Sum/solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ 
+public class B_Maximum_Multiple_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            if (n == 3)
+                System.out.println(3);
+            else
+                System.out.println(2);
+        }
+    }
+}
