a928675ae06a62a4892a990705597deb334c9e22
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:57:54 +0530

    Add Triple [B] from Codeforces

---
+++ 1669/B - Triple/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] freq = new int[n + 1];
+ 
+            int answer = -1;
+ 
+            for (int i = 0; i < n; i++) {
+                int x = sc.nextInt();
+                freq[x]++;
+ 
+                if (freq[x] == 3 && answer == -1) {
+                    answer = x;
+                }
+            }
+ 
+            System.out.println(answer);
+        }
+    }
+}
