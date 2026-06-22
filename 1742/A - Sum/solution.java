a35276a1221e5a1c5ee4201a4c12f3689795abed
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:34:41 +0530

    Add Sum [A] from Codeforces

---
+++ 1742/A - Sum/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+ 
+            if (a == b + c || b == a + c || c == a + b) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
