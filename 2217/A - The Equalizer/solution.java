0a5f287de6501b01ff99963c89c6cd565f66a90d
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:52:47 +0530

    Add The Equalizer [A] from Codeforces

---
+++ 2217/A - The Equalizer/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+ 
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += sc.nextInt();
+            }
+ 
+            if (sum % 2 == 1 || (n * k) % 2 == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
