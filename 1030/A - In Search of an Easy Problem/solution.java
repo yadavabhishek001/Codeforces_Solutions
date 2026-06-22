3e6eee580002504dae6534bcbee9fafec3aff175
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:33:28 +0530

    Add In Search of an Easy Problem [A] from Codeforces

---
+++ 1030/A - In Search of an Easy Problem/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String max = "EASY";
+        for(int i = 0; i<n ; i++){
+            int a = sc.nextInt();
+            if(a == 1){
+                max = "HARD";
+                break;
+            }
+        }
+        System.out.println(max);
+    }
+}
