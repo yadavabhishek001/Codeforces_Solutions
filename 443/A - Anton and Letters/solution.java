35a062c031908352e5249a944b454d85652525ae
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:50:57 +0530

    Add Anton and Letters [A] from Codeforces

---
+++ 443/A - Anton and Letters/solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.HashSet;
+ 
+public class A_Anton_and_Letters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        String s = sc.nextLine();
+ 
+        HashSet<Character> set = new HashSet<>();
+ 
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                set.add(ch);
+            }
+        }
+ 
+        System.out.println(set.size());
+    }
+}
