d72dc946312f2c3c809f91361a5edac0e83cbe0f
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:33:16 +0530

    Add Vanya and Fence [A] from Codeforces

---
+++ 677/A - Vanya and Fence/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int h = sc.nextInt();
+        int count = 0;
+        for(int i = 0; i<n ; i++){
+            int a = sc.nextInt();
+            if(a>h){
+                count += 2;
+            }
+            else{
+                count += 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
