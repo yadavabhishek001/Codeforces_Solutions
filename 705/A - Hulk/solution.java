d6c80567bc41103a6a07cf6e6d7bcf91ccc5e747
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:50:02 +0530

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class A_Hulk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+ 
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                System.out.print("I hate");
+            } else {
+                System.out.print("I love");
+            }
+ 
+            if (i == n) {
+                System.out.print(" it");
+            } else {
+                System.out.print(" that ");
+            }
+        }
+    }
+}
