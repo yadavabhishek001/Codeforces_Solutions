f8dfeb7c91b6d5c0c4d0a03cb699bc4310ad483a
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 19:01:58 +0530

    Add Free Ice Cream [A] from Codeforces

---
+++ 686/A - Free Ice Cream/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        long x = sc.nextLong();
+ 
+        int distressed = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            char ch = sc.next().charAt(0);
+            long d = sc.nextLong();
+ 
+            if (ch == '+') {
+                x += d;
+            } else {
+                if (x >= d) {
+                    x -= d;
+                } else {
+                    distressed++;
+                }
+            }
+        }
+ 
+        System.out.println(x + " " + distressed);
+    }
+}
