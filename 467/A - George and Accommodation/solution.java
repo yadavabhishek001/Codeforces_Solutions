c819931413dd07b91a90cd038f1eccbfa660487f
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:34:24 +0530

    Add George and Accommodation [A] from Codeforces

---
+++ 467/A - George and Accommodation/solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0; i<n ;i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            if(a <= b-2){
+               sum++; 
+            }
+        }
+        System.out.println(sum);
+    }
+}
