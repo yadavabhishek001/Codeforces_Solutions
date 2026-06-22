dd94a40bc146a5bb04a7af13855ff5e4a0706422
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:36:06 +0530

    Add Marathon [A] from Codeforces

---
+++ 1692/A - Marathon/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int count = 0;
+        for(int i = 0; i<t ;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+            count = 0;
+            if(b>a){
+                count++;
+            }
+            if(c>a){
+                count++;
+            }
+            if(d>a){
+                count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
