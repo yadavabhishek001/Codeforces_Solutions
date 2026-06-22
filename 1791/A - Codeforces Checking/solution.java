e01f7df5cc4204b85b17447e4f5204ea3977beab
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:36:36 +0530

    Add Codeforces Checking [A] from Codeforces

---
+++ 1791/A - Codeforces Checking/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        String s = "codeforces";
+        String ans = "";
+        for(int i = 0; i<t ;i++){
+            char a = sc.next().charAt(0);
+            for(int j = 0; j<10 ; j++ ){
+                ans = "NO";
+                if(s.charAt(j) == a){
+                    ans = "YES";
+                    break;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
