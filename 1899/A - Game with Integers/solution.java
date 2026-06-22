37d6e3ece49cd0d17912825022a142cc8bf18d8f
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:43:13 +0530

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class A_Game_with_Integers{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while(t-- > 0){
+            int n = sc.nextInt();
+ 
+            System.out.println( n%3 != 0 ? "First" : "Second" );
+        }
+    }
+}
