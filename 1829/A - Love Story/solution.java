058ef7f03aedd4ae845c335610fa0146e832cf78
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:39:58 +0530

    Add Love Story [A] from Codeforces

---
+++ 1829/A - Love Story/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        for(int i = 0; i<t ; i++){
+            String s1 = sc.next();
+            String s2 = "codeforces";
+            
+            int count = 0;
+            int n = s1.length();
+            
+            for(int j = 0; j<n ; j++){
+                if( s1.charAt(j) != s2.charAt(j) ){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+        
+        sc.close();
+    }
+}
