bf3245ec51c74fc7f0ec685fc40819eb0f074d72
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:40:47 +0530

    Add Petya and Strings [A] from Codeforces

---
+++ 112/A - Petya and Strings/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        s2 = s2.toLowerCase();
+        s1 = s1.toLowerCase();
+        
+        if( s1.compareTo(s2) < 0){
+            System.out.println("-1");
+        }
+        else if( s1.compareTo(s2) > 0){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+        sc.close();
+    }
+}
