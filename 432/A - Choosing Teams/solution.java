1779553c7c0a045b89e41408fdf7accdc782c70f
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:57:32 +0530

    Add Choosing Teams [A] from Codeforces

---
+++ 432/A - Choosing Teams/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int y = sc.nextInt();
+            if (y <= 5 - k) {
+                count++;
+            }
+        }
+        
+        System.out.println(count / 3);
+    }
+}
