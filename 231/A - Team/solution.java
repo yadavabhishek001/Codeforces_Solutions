1fd66498395e99a0770bd59601014fb93ae930da
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:49:25 +0530

    Add Team [A] from Codeforces

---
+++ 231/A - Team/solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class A_Team{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+ 
+        int count = 0;
+ 
+        for(int i = 0; i<n ; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+ 
+            if( (a == 1 && b == 1) || (b == 1 && c == 1) || (c == 1 && a == 1)){
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
