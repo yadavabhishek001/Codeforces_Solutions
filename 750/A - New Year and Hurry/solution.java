4efd5c9e4d2b770b14c65e3a95dc500e841b518f
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:56:12 +0530

    Add New Year and Hurry [A] from Codeforces

---
+++ 750/A - New Year and Hurry/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        int timeLeft = 240 - k;
+        int totalTime = 0;
+        int solved = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            totalTime += 5 * i;
+            
+            if (totalTime > timeLeft) {
+                break;
+            }
+            
+            solved++;
+        }
+        
+        System.out.println(solved);
+    }
+}
