ab397d0cd23d47a3a97771df35bacb8d71f7bbaa
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:38:53 +0530

    Add Anton and Danik [A] from Codeforces

---
+++ 734/A - Anton and Danik/solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        
+        int countA = 0;
+        int countD = 0;
+        for(int i = 0; i<n ; i++){
+            if( str.charAt(i) == 'A' ){
+                countA++;
+            }
+            else{
+                countD++;
+            }
+        }
+        
+        if(countA>countD){
+            System.out.println("Anton");
+        }
+        else if(countA == countD){
+            System.out.println("Friendship");
+        }
+        else{
+            System.out.println("Danik");
+        }
+        sc.close();
+    }
+}
