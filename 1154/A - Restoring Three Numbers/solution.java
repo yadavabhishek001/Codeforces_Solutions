6947736a12a64c1e274d2b477903e8cadc6225bc
Abhishek Yadav <abhasnp55@gmail.com>
2026-06-22 18:31:43 +0530

    Add Restoring Three Numbers [A] from Codeforces

---
+++ 1154/A - Restoring Three Numbers/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class main{
+    public static void main (String args[]){
+     Scanner sc = new Scanner(System.in);
+     int arr[] = new int[4];
+     int max = 0;
+     int a = -1;
+     for( int i = 0; i < 4 ; i++){
+         arr[i] = sc.nextInt();
+         if(arr[i] > max){
+             max = arr[i];
+             a = i;
+         }
+     }
+     for(int i=0 ; i<4 ; i++){
+         if(i == a){
+             continue;
+         }
+         System.out.print((max - arr[i]) + " ");
+     }
+    }
+}
